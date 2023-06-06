3c0052167893832aa884ede9b00da452d599f836
Can <can.kayikci@ug.bilkent.edu.tr>
2023-06-06 20:48:44 +0300

    Table models #20

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
 </head>
 
 <body data-bs-theme="dark">
-
+    <?php
+        require_once "core/db.php";
+        require_once "models/users.model.php";
+        $a = new UsersModel($db);
+        
+        $name = "John";
+        $last_name = "Doe";
+        $email = "john.aadoe@example.com";
+        $birth_date = "1990-01-01";
+        $picture = "profile.jpg";
+        $password = "password123";
+        // $a->create($name, $last_name, $email, $birth_date, $picture, $password);   
+    ?>
     <?php include("core/scripts.php"); ?>
 </body>
 
